da3fae5b8788c851618228cc56bf4be7d6c8e1aa
KameDev <kamedev02@users.noreply.github.com>
2026-06-22 00:03:20 +0700

    chore: update icons

--- gui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import package_info_plus
 import screen_retriever_macos
 import shared_preferences_foundation
 import system_tray
+import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -16,5 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SystemTrayPlugin.register(with: registry.registrar(forPlugin: "SystemTrayPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
